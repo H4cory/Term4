d454d2cfc25efb631fbf407d0d1e338642677cca
H4coryLX <k.kanev13@gmail.com>
2023-05-09 15:59:11 +0300

    a

---
+++ .vscode/alive/fasl/tmp.lisp
@@ -0,0 +1,17 @@
+(defvar ABC `((TheRain DMX) (TheRain OranJuiceJones) (Disturbia Rihana)))
+
+(write (CAR ABC))
+(fresh-line)
+(write (CADDR ABC))
+(fresh-line)
+(write (CAAR ABC))
+(fresh-line)
+(write (CADADR ABC))
+(fresh-line)
+(write (CAADDR ABC))
+(fresh-line)
+(write (CDADDR ABC))
+(fresh-line)
+
+
+
---
+++ PY/26.04.23/Test1.py
@@ -0,0 +1,7 @@
+myInt = [1,2,3,4,5,6,7,8,9,10]
+i = 0
+while i != 10 :
+    print(myInt[i])
+    print(" ")
+    i = i + 1
+print("END")
